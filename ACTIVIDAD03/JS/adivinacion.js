cae984c31918fb229bcb5e58d6988ede5ed17a00
ALUMNADO\eudoal <halleck.g@gmail.com>
2017-10-03 13:28:45 +0200

    subiendo act03

---
+++ ACTIVIDAD03/JS/adivinacion.js
@@ -0,0 +1,10 @@
+/* script para divinar un numeno
+*/
+var adivinar=55;
+var n1 = prompt("Dime el numero que estoy pensando");
+if (adivinar==n1) {
+alert("Es correcto!!")}
+if (adivinar>n1) {
+alert("Te has quedado corto")}
+if (adivinar<n1) {
+alert("T'as paso!!!")}
