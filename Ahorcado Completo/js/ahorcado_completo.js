1e51d2f326ec30da273e88f6aa4be395fb9e80d0
Halleck_Lenovo\Prueba <halleck.g.florida@gmail.com>
2017-11-14 13:35:32 +0100

    Ahorado completo

---
+++ Ahorcado Completo/js/ahorcado_completo.js
@@ -0,0 +1,75 @@
+// creo un Array de las palabras que quiero utilizar
+var palabras=["coche","moto","camioneta","motocultor","carretilla"];
+//creo una variable con la cantidad de palabras que hay en el array
+var letras=palabras.length;
+// creo un random con el numero de palabras del array
+palabra_Adivinar=Math.floor(Math.random()*letras);
+// creo una variable que contenga el numero de letras de la palabra elegida al azar
+var letras_Palabra=(palabras[palabra_Adivinar]).length;
+// variable con un valr cualquiera para utilizarla más tarde
+var cadena_palabra="valor"
+//variable de los intentos
+var int=4;
+
+console.log(letras+ " es el numero de palabras del array");
+console.log(palabras[palabra_Adivinar]+ " es palabra elegida aleatoriamente");
+console.log(letras_Palabra+ " es el numero de letras de la palabra");
+console.log(palabra_Adivinar+ " es la posicion de la palabra en el array");
+
+//subtitulle las letras de la palabra elegida por ceros
+cadena_palabra = palabras[palabra_Adivinar].replace(/./g,"0");
+
+//fiincion para reemplazar la letra
+function replaceAt(pos,caracterIn,texto){
+  let textTemp=texto.substr(0,pos);
+  textTemp=textTemp+caracterIn;
+  textTemp=textTemp+texto.substr(pos+1,(texto.length-1-pos));
+
+  return textTemp;
+}
+//guardo cadena_palabra en la variable texto
+var texto=cadena_palabra;
+
+// hará el DO mientras texto sea diferenta a palabra aleatoria y además el numero de intentos sea mayor o igual a 0
+do {
+  var conta=0;
+  var letra_introducida = prompt("Adivina la letra");
+
+console.log(texto+ "  texto");
+for (var i = letras_Palabra; i >= 1; i--) {
+
+//recorre cada letra de la palabra
+  var str = palabras[palabra_Adivinar];
+  var res = str.charAt(str.length-i);
+
+  console.log(res);
+  console.log(str);
+  console.log(i);
+
+// si la letra introducida es igual a la letra recorrida "res" la sustitulle
+ if (letra_introducida==res){
+   conta++;
+texto=replaceAt(letras_Palabra-(i),res,texto);
+console.log(conta+ "num letras inguales al final del do");
+console.log(texto);
+}
+
+//si el contador llega a 0 y a termina de recorrer la palabra muestra el alert y descuenta un intento
+ if ((conta==0)&&(i==1)){
+   alert("MAAALLLL, INTENTOS RESTANTES... "+ int);
+   int--;
+ }
+}
+console.log(texto);
+alert(texto)
+
+
+} while ((texto!=palabras[palabra_Adivinar])&&(int>=0));
+
+
+if (int<0) {
+  alert("SER TERMINARON TUS INTENTOS");
+}
+else {
+  alert("welll doooone");
+}
